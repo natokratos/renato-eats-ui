970d517f937d444347ef8b7c696aca134916d1bd
natokratos <micro8696@caelum114-04>
2019-10-28 12:54:57 -0300

    exercicios

--- src/app/services/pagamento.service.ts
+++ src/app/services/pagamento.service.ts
@@ -9,21 +9,32 @@ import { environment } from 'src/environments/environment';
 })
 export class PagamentoService {
 
-  private API = environment.baseUrl + '/pagamentos';
+  private API = environment.pagamentoUrl + '/pagamentos';
 
   constructor(private http: HttpClient) {
   }
 
   cria(pagamento): Observable<any> {
+    console.log("cria pagamento", pagamento);
+    this.ajustaIds(pagamento);
+    console.log("cria pagamento", pagamento);
     return this.http.post(`${this.API}`, pagamento);
   }
 
   confirma(pagamento): Observable<any> {
+    this.ajustaIds(pagamento);
     return this.http.put(`${this.API}/${pagamento.id}`, null);
   }
 
   cancela(pagamento): Observable<any> {
+    this.ajustaIds(pagamento);
     return this.http.delete(`${this.API}/${pagamento.id}`);
+  }
+
+  // adicionado
+  private ajustaIds(pagamento) {
+    pagamento.formaDePagamentoId = pagamento.formaDePagamentoId || pagamento.formaDePagamento.id;
+    pagamento.pedidoId = pagamento.pedidoId || pagamento.pedido.id;
   }
 
 }
